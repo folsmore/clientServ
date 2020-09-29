77c9411c539d980f52a92f429ee67926290e5ebc
folsmore <69353018+folsmore@users.noreply.github.com>
2020-09-29 09:46:57 +0300

    Add files via upload

---
+++ ServMes/ServMes/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace ServMes
+{
+    public class ClientObject
+    {
+        public TcpClient client;
+        public ClientObject(TcpClient tcpClient)
+        {
+            client = tcpClient;
+        }
+        public static bool IsPalindrome(string value) // Проверка на палиндром
+        {
+          
+           
+            int min = 0;
+            int max = value.Length - 1;
+            while (true)
+            {
+                if (min > max)
+                {
+                    return true;
+                }
+                char a = value[min];
+                char b = value[max];
+                if (char.ToLower(a) != char.ToLower(b))
+                {
+                    return false;
+                }
+                min++;
+                max--;
+            }
+        }
+
+
+        public void Process() // Получение, разкодировка, обработка строки от пользователя
+        {
+            NetworkStream stream = null;
+            try
+            {
+                stream = client.GetStream();
+                byte[] data = new byte[64]; // буфер для получаемых данных
+                while (true)
+                {
+                    // получаем сообщение
+                    StringBuilder builder = new StringBuilder();
+                    int bytes = 0;
+                    do
+                    {
+                        bytes = stream.Read(data, 0, data.Length);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    }
+                    while (stream.DataAvailable);
+
+                    string message = builder.ToString();
+
+                    Console.WriteLine(message);
+                    // отправляем обратно сообщение в верхнем регистре
+                    message = message.Substring(message.IndexOf(':') + 1).Trim().Replace(" ", "");
+
+                    message = Convert.ToString(IsPalindrome(message));
+                    if (message == "True")
+                    {
+                        message = "Строка является палиндромом";
+                    }
+                    else 
+                    {
+                        message = "Строка не является палиндромом";
+                    }
+                    data = Encoding.Unicode.GetBytes(message);
+                    stream.Write(data, 0, data.Length);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (stream != null)
+                    stream.Close();
+                if (client != null)
+                    client.Close();
+            }
+        }
+    }
+
+class Program
+{
+    const int port = 8888;
+    static TcpListener listener;
+    static void Main(string[] args)
+    {
+        try
+        {
+            listener = new TcpListener(IPAddress.Parse("127.0.0.1"), port);
+            listener.Start();
+            Console.WriteLine("Ожидание подключений...");
+
+            while (true)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                ClientObject clientObject = new ClientObject(client);
+
+                // создаем новый поток для обслуживания нового клиента
+                Thread clientThread = new Thread(new ThreadStart(clientObject.Process));
+                clientThread.Start();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            if (listener != null)
+                listener.Stop();
+        }
+    }
+}
+}
